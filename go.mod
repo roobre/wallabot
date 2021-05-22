f8ffb676d3eb4baa15f77a68856984c70fcf6c7f
Roberto Santalla <roobre@roobre.es>
2021-05-22 19:15:31 +0200

    wip: implement v3 api

---
+++ go.mod
@@ -0,0 +1,5 @@
+module roob.re/wallabot
+
+go 1.16
+
+require github.com/google/go-querystring v1.1.0
---
+++ wallapop/models.go
@@ -0,0 +1,39 @@
+package wallapop
+
+const searchUrl = "/general/search"
+
+type searchResponse struct {
+	Items []Item `json:"search_objects"`
+
+	From int `json:"from"`
+	To   int `json:"to"`
+}
+
+type SearchArgs struct {
+	Keywords  string  `url:"keywords"`
+	Latitude  float64 `url:"latitude"`
+	Longitude float64 `url:"longitude"`
+	OrderBy   string  `url:"order_by,omitempty"`
+	Language  string  `url:"language,omitempty"`
+	Urgent    bool    `url:"urgent,omitempty"`
+	Shipping  bool    `url:"shipping,omitempty"`
+	Exchange  bool    `url:"exchange,omitempty"`
+}
+
+type Item struct {
+	ID string `json:"id"`
+
+	Title       string `json:"title"`
+	Description string `json:"description"`
+
+	Images []ItemImage `json:"images"`
+
+	Price    float64 `json:"price"`
+	Currency string  `json:"currency"`
+
+	Slug string `json:"web_slug"`
+}
+
+type ItemImage struct {
+	OriginalURL string `json:"original"`
+}
