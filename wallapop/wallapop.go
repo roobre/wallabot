96409d88ed7002f36e9438eaa402dce7aad5e3b1
Roberto Santalla <roobre@roobre.es>
2021-05-26 21:14:59 +0200

    wallapop: implement multi-page search

--- wallapop/wallapop.go
+++ wallapop/wallapop.go
@@ -37,9 +37,15 @@ func (c *Client) Search(args SearchArgs) ([]Item, error) {
 	var nextPageParams string
 
 	for page := 0; page < args.Pages; page++ {
-		response, err := c.http.Request(searchPath+"?"+nextPageParams, http.MethodGet, args)
+		url := searchPath + "?" + nextPageParams
+		response, err := c.http.Request(url, http.MethodGet, args)
 		if err != nil {
 			return nil, fmt.Errorf("could not make http request: %w", err)
+		}
+		defer response.Body.Close()
+
+		if response.StatusCode != 200 {
+			return nil, fmt.Errorf("server responded with %d to %s", response.StatusCode, url)
 		}
 
 		sr := &searchResponse{}
